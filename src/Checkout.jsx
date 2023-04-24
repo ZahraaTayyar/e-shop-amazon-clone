b34c24186f7615a4bedf1222c103705bce6fcd19
ZahraaTayyar <zahraatayyar@gmail.com>
2023-04-24 12:36:50 +0100

    checkout css

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal.jsx";
 
 function Checkout() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="checkout">
+      <div className="checkout-left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+          className="checkout-ad"
+        />
+
+        <div>
+          <h2 className="checkout-title">Your Shopping Basket</h2>
+          <CheckoutProduct />
+          <CheckoutProduct />
+          <CheckoutProduct />
+        </div>
+      </div>
+
+      <div className="checkout-right">
+        <Subtotal />
+      </div>
     </div>
   );
 }
---
+++ src/CheckoutProduct.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+function CheckoutProduct() {
+  return (
+    <div className="checkoutProduct">
+      <img
+        src="image-link"
+        alt="checkout Product"
+        className="checkoutProduct-image"
+      ></img>
+
+      <div className="checkoutProduct-info">
+        <p className="checkoutProduct-title">
+          Bennet Mystic 15.6 inch Laptop Shoulder Messenger SLing Office Bag,
+          Water Repellent Fabric for Men and Women (Blue)
+        </p>
+
+        <p className="checkoutProduct-price">
+          <small>$</small>
+          <strong>30</strong>
+        </p>
+        <div className="checkoutProduct-rating">⭐⭐</div>
+        <button>Remove from Basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
