1a576cae5f5d21a8d3a780932fd9b0ec074f50eb
ZahraaTayyar <zahraatayyar@gmail.com>
2023-05-02 12:48:49 +0100

    Login page done

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,9 +1,12 @@
+import { SportsBasketball } from "@mui/icons-material";
 import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
 import Subtotal from "./Subtotal.jsx";
+import { useStateValue } from "./StateProvider";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout-left">
@@ -15,9 +18,15 @@ function Checkout() {
 
         <div>
           <h2 className="checkout-title">Your Shopping Basket</h2>
-          <CheckoutProduct />
-          <CheckoutProduct />
-          <CheckoutProduct />
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            ></CheckoutProduct>
+          ))}
         </div>
       </div>
 
